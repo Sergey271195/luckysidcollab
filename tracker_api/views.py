cdaf1ef47ab6e52acdc0ec15f7b58b4a4df1256d
Sergey <mackovckin.sergei@yandex.ru>
2020-05-24 13:12:22 +0300

    setting webhooks and basic logic localhost

--- tracker_api/views.py
+++ tracker_api/views.py
@@ -9,8 +9,12 @@ from rest_framework import status
 
 import requests
 import os
+import json
+import re
+
 
 from .models import BotUserSerializer, BotUserProfileSerializer, ExpenseSerializer, BotUser, BotUserProfile, Expense
+
 
 
 class TelegramBot():
@@ -35,10 +39,10 @@ class TelegramBot():
         tracker_update = telegram_response.json()
         return tracker_update
 
-    def sendMessage(self, user_id, text):
+    def sendMessage(self, user_id, text, parse_mode = ''):
 
         send_message_url = os.path.join(self.url, 'sendMessage')
-        telegram_request = requests.post(send_message_url, data = {'chat_id': user_id, 'text': text})
+        telegram_request = requests.post(send_message_url, data = {'chat_id': user_id, 'text': text, 'parse_mode': parse_mode})
         tracker_message= telegram_request.json()
         return tracker_message
 
@@ -51,10 +55,90 @@ class TelegramBot():
 
         return unique_ids
 
-class MainApiView(View):
+class MainApiView(APIView):
 
-    def get(request, *args, **kwargs):
+    tgBot = TelegramBot()
+
+    def checkUser(self, notification):
+
+        user_id = notification['message']['from'].get('id')
+
+        if (not BotUser.objects.filter(telegram_id = int(user_id)).exists()):
+            new_user = BotUser(telegram_id = int(user_id))
+            new_user.save()
+            print('New user has been created')
+            self.tgBot.sendMessage(user_id, "Greetings and welcome. You've been added to our small db. Wish you a nice stay. If you have any complaints, contact LuckySid! ;)")
+
+        user = BotUser.objects.get(telegram_id = int(user_id))
+        return user
+
+    def messageHandler(self, notification, user):
+
+        message = notification['message'].get('text')
+
+        if message == 'data':
+            print('Data request')
+            expenses = BotUserProfile.objects.get(user = user).expenses.all()
+            for expense in expenses:
+                response = ''
+                response += f'Date: {expense.date} \n'
+                response += f'Amount: {expense.amount} \n'
+                response += f'Category: {expense.get_category_display()} \n'
+                self.tgBot.sendMessage(user.telegram_id, response, 'HTML')
+
+        elif message.find('send') != -1:
+
+            amount = re.search(r'amount:(\s)?(?P<amount>(\d+)(.(\d+))?)', message)
+            category = re.search(r'category:(\s)?(?P<category>(\w{2}\b))', message)           
+
+            if amount and category:
+                post_dict = {'amount': amount.group('amount'), 'category': category.group('category').upper()}
+                serializer = ExpenseSerializer(data = post_dict)
+                if serializer.is_valid():
+                    expense = Expense(amount = post_dict.get('amount'), category = post_dict.get('category'))
+                    expense.save()
+                    BotUserProfile.objects.get(user = user).expenses.add(expense)
+                    self.tgBot.sendMessage(user.telegram_id, "Your data has been added to db!")
+                else:
+                    print(serializer.errors)
+                    self.tgBot.sendMessage(user.telegram_id, "You've been providing wrong data!")
+
+            else:
+                self.tgBot.sendMessage(user.telegram_id, "You've been providing wrong data!")
+
+        elif message == 'help':
+
+            categories = 'Available categories: \n FD (FOOD) \n SV (SERVICES) \n AP (APPLIANCES) \n PT (PETROL) \n RT (RESTAURANTS)\n'
+            methods = "Available methods: \n data - get list of your expenses \n send amount: (money you've spent) category: (one of the available categories) - to add data to database \n"
+            check = "check users - just for informational purposes"
+            self.tgBot.sendMessage(user.telegram_id, categories+methods+check, 'HTML')
+
+        elif message == 'check users':
+            users = BotUser.objects.all()
+            response = ''
+            for u in users:
+                response += f'{u.telegram_id} \n'
+
+            self.tgBot.sendMessage(user.telegram_id, response, 'HTML')
+
+
+
+    def get(self, request, *args, **kwargs):
         return(HttpResponse('Tracker Api!'))
+
+    @csrf_exempt
+    def post(self, request, *args, **kwargs):
+
+        notification = json.loads(request.body)
+        
+        user = self.checkUser(notification)
+
+        self.messageHandler(notification, user)
+        
+        print(user)
+        print(notification)
+
+        return(JsonResponse({'status_code': 200}))
 
 
 class TelegramConnectionView(TemplateView):
